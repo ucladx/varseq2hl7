ba95e5e54f860910c8b19adacc1106ded47aa7bf
Ian Atol <iatol@mednet.ucla.edu>
2023-08-24 08:56:19 -0700

    rm wet lab metrics, some fixes for EPIC formatting

--- varseq_hl7.py
+++ varseq_hl7.py
@@ -1,6 +1,7 @@
 import hl7.client
 from flask import Flask, request, jsonify
 from datetime import date
+from textwrap import wrap
 
 # creates unique variant IDs that Beaker likes in the following pattern:
 # 2a, 2b, ..., 2z, 2A, 2B, ..., 2Z, 2aa, 2ab, ..., 2aZ, 2ba, 2bb
@@ -72,18 +73,6 @@ class VarSeqInfo():
         self.norm_order_num = self.get_custom_field("N_OrderID")
         self.norm_date_ordered = self.format_msh_date(self.get_custom_field("N_DateOrdered").split(" ")[0])
         self.norm_date_received = self.format_msh_date(self.get_custom_field("N_DateReceived").split(" ")[0])
-        self.lib_avg_size = self.get_custom_field("lib_avg_size")
-        self.lib_conc = self.get_custom_field("lib_conc")
-        self.nanodrop_conc = self.get_custom_field("nanodrop_conc")
-        self.qubit_conc = self.get_custom_field("qubit_conc")
-        self.pool_lib_conc = self.get_custom_field("pool_lib_conc")
-        self.pool_lib_avg_size = self.get_custom_field("pool_lib_avg_size")
-        self.N_lib_avg_size = self.get_custom_field("N_lib_avg_size")
-        self.N_lib_conc = self.get_custom_field("N_lib_conc")
-        self.N_nanodrop_conc = self.get_custom_field("N_nanodrop_conc")
-        self.N_qubit_conc = self.get_custom_field("N_qubit_conc")
-        self.N_pool_lib_conc = self.get_custom_field("N_pool_lib_conc")
-        self.N_pool_lib_avg_size = self.get_custom_field("N_pool_lib_avg_size")
     
     def get_obx_idx(self):
         self.obx_idx += 1
@@ -102,7 +91,10 @@ class VarSeqInfo():
         return list(germline_biomarkers) + self.varseq_json["germlineVariants"]
 
     def get_all_variants(self):
-        return self.get_biomarkers() + self.varseq_json["germlineVariants"] + self.varseq_json["uncertainVariants"]
+        all_variants = self.get_biomarkers() + self.varseq_json["germlineVariants"] + self.varseq_json["uncertainVariants"]
+        variant_vaf_dict = {variant: variant["vaf"] for variant in all_variants}
+        return sorted(all_variants, key=lambda variant: variant_vaf_dict[variant], reverse=True)
+        # return self.get_biomarkers() + self.varseq_json["germlineVariants"] + self.varseq_json["uncertainVariants"]
 
     def get_sig(self, sig_name):
         for biomarker in self.varseq_json["biomarkers"]:
@@ -159,13 +151,17 @@ class VarSeqInfo():
         else:
             return "Unknown"
 
+    # TODO test if EPIC can do this automatically with HTML tags
+    def insert_newlines(self, s, every=110):
+        return '\.br\\'.join(wrap(s, every))
+
     def get_interp(self, variant):
         summary = variant["biomarkerSummary"]
         if summary:
             interp = summary["interpretation"]
             # TODO if we end up seeing more HTML elements, actually do this properly with regex (re.sub)
             interp = interp.replace("<p>", "").replace("</p>", "").replace("&nbsp;", " ")
-            return interp
+            return self.insert_newlines(interp)
         else:
             return ""
 
@@ -175,7 +171,26 @@ class VarSeqInfo():
             return mutation["chr"]
         else:
             return ""
-        
+
+    # TODO replace with map from sequenceOntology -> EPIC term
+    def get_effect(self, variant):
+        mutation = variant["mutation"]
+        if mutation:
+            effect = mutation["effect"].replace("In-frame", "inframe")
+            return effect
+        else:
+            return ""
+ 
+    def get_naf(self, variant):
+        naf = variant["naf"]
+        if naf == "null":
+            return "<LoD"
+        rounded_naf = round(naf, 2)
+        if rounded_naf == 0:
+            return "<LoD"
+        else:
+            return rounded_naf
+
     def get_variant_obx(self, variant_id, loinc_code, value):
         return f"""OBX|{self.get_obx_idx()}|{get_loinc_info(loinc_code)}|{variant_id}|{value}"""
 
@@ -219,7 +234,7 @@ class VarSeqInfo():
             get_variant_obx("81252-9", f"v1^{variant['hgvsWithGene']}^ClinVar-V"), # Discrete Genetic Variant
             get_variant_obx("48018-6", f"^{display_name}^"), # Variant Name (This is also a discrete field in EPIC)
             get_variant_obx("62374-4", "^GRCh38"), # Human Reference Sequence Assembly Version
-            get_variant_obx("81258-6", round(variant['naf'], 2)), # Allelic Frequency
+            get_variant_obx("81258-6", self.get_naf(variant)), # Allelic Frequency
             get_variant_obx("69548-6", "Detected"), # Genetic Variant Assessment
             get_variant_obx("48002-0", f"^{self.get_variant_type(variant)}"), # Genomic Source Class
         ]
@@ -233,26 +248,14 @@ ORC|RE\r
 OBR|1|{self.order_num}|{self.sample_id}^Beaker|LAB9055^Pan-cancer Solid Tumor Panel^BKREAP^^^^^^SOLID TUMOR PAN-CANCER PANEL|||{self.date_ordered}|||||||||{self.prov_id}^{self.prov_ln}^{self.prov_fn}^^^^^^EPIC^^^^PROVID||||||{self.date_received}|||F\r
 {self.get_variant_obx("2a", "7102415", f"^{self.get_tumor_type()}")}\r
 {self.get_variant_obx("2a", "81695-9", f"^{self.get_msi()}")}\r
-{self.get_variant_obx("2a", "94076-7", f"{self.get_tmb()}")}\r
-{self.get_variant_obx("2a", "7102416", f"^{self.lib_avg_size}")}\r
-{self.get_variant_obx("2a", "7102417", f"^{self.lib_conc}")}\r
-{self.get_variant_obx("2a", "7102418", f"^{self.nanodrop_conc}")}\r
-{self.get_variant_obx("2a", "7102419", f"^{self.qubit_conc}")}\r
-{self.get_variant_obx("2a", "7102420", f"^{self.pool_lib_conc}")}\r
-{self.get_variant_obx("2a", "7102421", f"^{self.pool_lib_avg_size}")}\r"""
+{self.get_variant_obx("2a", "94076-7", f"{self.get_tmb()}")}\r"""
 
     def get_normal_hl7_header(self):
         self.reset_obx_idx()
         return f"""MSH|^~\&|RRH||Beaker||{self.date_sent}||ORU^R01|1|P|2.3||||||\r
 PID|1||{self.mrn}^^^MRN^MRN||{self.pt_ln}^{self.pt_fn}^||{self.bday}|{self.sex}\r
 ORC|RE\r
-OBR|1|{self.norm_order_num}|{self.norm_sample_id}^Beaker|LAB9056^Pan-cancer Panel, Comparator^BKREAP^^^^^^SOLID TUMOR PAN-CANCER PANEL|||{self.norm_date_ordered}|||||||||{self.prov_id}^{self.prov_ln}^{self.prov_fn}^^^^^^EPIC^^^^PROVID||||||{self.norm_date_received}|||F\r
-{self.get_variant_obx("2a", "7102416", f"^{self.N_lib_avg_size}")}\r
-{self.get_variant_obx("2a", "7102417", f"^{self.N_lib_conc}")}\r
-{self.get_variant_obx("2a", "7102418", f"^{self.N_nanodrop_conc}")}\r
-{self.get_variant_obx("2a", "7102419", f"^{self.N_qubit_conc}")}\r
-{self.get_variant_obx("2a", "7102420", f"^{self.N_pool_lib_conc}")}\r
-{self.get_variant_obx("2a", "7102421", f"^{self.N_pool_lib_avg_size}")}\r"""
+OBR|1|{self.norm_order_num}|{self.norm_sample_id}^Beaker|LAB9056^Pan-cancer Panel, Comparator^BKREAP^^^^^^SOLID TUMOR PAN-CANCER PANEL|||{self.norm_date_ordered}|||||||||{self.prov_id}^{self.prov_ln}^{self.prov_fn}^^^^^^EPIC^^^^PROVID||||||{self.norm_date_received}|||F\r"""
 
     def get_hl7_variants(self):
         variants = self.get_all_variants()
@@ -276,7 +279,7 @@ def create_hl7_msgs(vs_json):
 def send_hl7_msgs(vs_json):
     tumor_msg, normal_msg = create_hl7_msgs(vs_json)
     hostname = "interface-test.mednet.ucla.edu"
-    port = 7199
+    port = 17199
 
     # with open("./hl7_msg_N.txt", "w") as f:
     #     f.write(normal_msg)
